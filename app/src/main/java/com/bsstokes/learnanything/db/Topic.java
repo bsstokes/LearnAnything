0d3fc8f297dd6692b931d2d7c4913f315184eb6c
Brian Stokes <bsstokes@gmail.com>
2015-02-11 16:20:17 -0600

    Add Topic RealmObject.

---
+++ app/src/main/java/com/bsstokes/learnanything/db/Topic.java
@@ -0,0 +1,45 @@
+package com.bsstokes.learnanything.db;
+
+import io.realm.RealmObject;
+import io.realm.annotations.Index;
+
+public class Topic extends RealmObject {
+    @Index
+    private String id;
+    private String title;
+    @Index
+    private String slug;
+    private boolean topLevel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public boolean isTopLevel() {
+        return topLevel;
+    }
+
+    public void setTopLevel(boolean topLevel) {
+        this.topLevel = topLevel;
+    }
+}
