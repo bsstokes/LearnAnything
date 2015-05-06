43f60827b8ce40133c80e99d76292464923e88f5
Brian Stokes <bsstokes@gmail.com>
2015-05-06 08:16:05 -0500

    Create Child model.

---
+++ app/src/main/java/com/bsstokes/learnanything/models/Child.java
@@ -0,0 +1,62 @@
+package com.bsstokes.learnanything.models;
+
+import android.support.annotation.Nullable;
+
+import auto.parcel.AutoParcel;
+
+@AutoParcel
+public abstract class Child {
+
+    @Nullable
+    public abstract String getId();
+
+    @Nullable
+    public abstract String getKind();
+
+    public abstract boolean isHidden();
+
+    @Nullable
+    public abstract String getKey();
+
+    @Nullable
+    public abstract String getInternalId();
+
+    @Nullable
+    public abstract String getTitle();
+
+    @Nullable
+    public abstract String getUrl();
+
+    @Nullable
+    public abstract String getTranslatedTitle();
+
+    @Nullable
+    public abstract String getNodeSlug();
+
+    public static Builder builder() {
+        return new AutoParcel_Child.Builder();
+    }
+
+    @AutoParcel.Builder
+    public interface Builder {
+        Builder id(String id);
+
+        Builder kind(String kind);
+
+        Builder hidden(boolean hidden);
+
+        Builder key(String key);
+
+        Builder internalId(String internalId);
+
+        Builder title(String title);
+
+        Builder url(String url);
+
+        Builder translatedTitle(String translatedTitle);
+
+        Builder nodeSlug(String nodeSlug);
+
+        Child build();
+    }
+}
