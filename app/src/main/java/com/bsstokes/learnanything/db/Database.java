3e15e5cca01dbe9409d1af76cdd71eab6243997b
Brian Stokes <bsstokes@gmail.com>
2015-05-06 16:08:34 -0500

    Reorganize Database.

--- app/src/main/java/com/bsstokes/learnanything/db/Database.java
+++ app/src/main/java/com/bsstokes/learnanything/db/Database.java
@@ -6,15 +6,15 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.bsstokes.learnanything.data.Article;
+import com.bsstokes.learnanything.data.Child;
+import com.bsstokes.learnanything.data.Exercise;
+import com.bsstokes.learnanything.data.Topic;
+import com.bsstokes.learnanything.data.Video;
 import com.bsstokes.learnanything.data.transformers.ArticleToContentValues;
 import com.bsstokes.learnanything.data.transformers.ChildToContentValues;
 import com.bsstokes.learnanything.data.transformers.ExerciseToContentValues;
 import com.bsstokes.learnanything.data.transformers.TopicToContentValues;
 import com.bsstokes.learnanything.data.transformers.VideoToContentValues;
-import com.bsstokes.learnanything.data.Child;
-import com.bsstokes.learnanything.data.Exercise;
-import com.bsstokes.learnanything.data.Topic;
-import com.bsstokes.learnanything.data.Video;
 import com.squareup.phrase.Phrase;
 import com.squareup.sqlbrite.SqlBrite;
 
@@ -53,16 +53,18 @@ public class Database implements Closeable {
     }
 
     public Observable<SqlBrite.Query> getChildren(@NonNull String parentId) {
+
+        final String queryTemplate = "SELECT * FROM {table} WHERE {parent_id_column} = ?";
+
         final String table = TableConfig.Children.TABLE;
-        String queryTemplate = "SELECT * FROM {table} WHERE {parent_id_column} = ?";
-        String query = Phrase.from(queryTemplate)
+        final String query = Phrase.from(queryTemplate)
                 .put("table", table)
                 .put("parent_id_column", TableConfig.Children.COLUMN_PARENT_ID)
                 .format().toString();
 
         logQuery(query);
 
-        return getSqlBrite().createQuery(TableConfig.Children.TABLE, query, parentId);
+        return getSqlBrite().createQuery(table, query, parentId);
     }
 
     // Exercises
@@ -78,12 +80,17 @@ public class Database implements Closeable {
     }
 
     public Observable<SqlBrite.Query> getTopLevelTopics() {
-        final String table = TableConfig.Topics.TABLE;
+
         final String queryTemplate = "SELECT * FROM {table} WHERE {top_level_column} = 1";
-        String query = Phrase.from(queryTemplate)
+
+        final String table = TableConfig.Topics.TABLE;
+        final String query = Phrase.from(queryTemplate)
                 .put("table", table)
                 .put("top_level_column", TableConfig.Topics.COLUMN_TOP_LEVEL)
                 .format().toString();
+
+        logQuery(query);
+
         return getSqlBrite().createQuery(table, query);
     }
 
@@ -94,13 +101,30 @@ public class Database implements Closeable {
     }
 
     public Observable<SqlBrite.Query> getVideoByReadableId(String readableId) {
-        String query = "SELECT * FROM " + TableConfig.Videos.TABLE + " WHERE " + TableConfig.Videos.COLUMN_READABLE_ID + " = ?";
-        return getSqlBrite().createQuery(TableConfig.Videos.TABLE, query, readableId);
+
+        final String queryTemplate = "SELECT * FROM {table} WHERE {readable_id_column} = ?";
+
+        final String table = TableConfig.Videos.TABLE;
+        final String query = Phrase.from(queryTemplate)
+                .put("table", table)
+                .put("readable_id_column", TableConfig.Videos.COLUMN_READABLE_ID)
+                .format().toString();
+
+        logQuery(query);
+
+        return getSqlBrite().createQuery(table, query, readableId);
     }
 
     public Observable<SqlBrite.Query> getAllVideos() {
-        String query = "SELECT * FROM " + TableConfig.Videos.TABLE;
-        return getSqlBrite().createQuery(TableConfig.Videos.TABLE, query);
+
+        final String queryTemplate = "SELECT * FROM {table}";
+
+        final String table = TableConfig.Videos.TABLE;
+        final String query = Phrase.from(queryTemplate).put("table", table).format().toString();
+
+        logQuery(query);
+
+        return getSqlBrite().createQuery(table, query);
     }
 
     // Helpers
