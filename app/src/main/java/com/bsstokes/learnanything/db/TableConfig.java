40a68a94056988029fc132ee53471a335f4542d8
Brian Stokes <bsstokes@gmail.com>
2015-05-06 08:16:49 -0500

    Change ID to String because that's how the API does it.

--- app/src/main/java/com/bsstokes/learnanything/db/TableConfig.java
+++ app/src/main/java/com/bsstokes/learnanything/db/TableConfig.java
@@ -4,7 +4,7 @@ public class TableConfig {
 
     public static class Videos {
         public static final String TABLE = "videos";
-        public static final String COLUMN_ID = "_id";
+        public static final String COLUMN_ID = "id";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_HTML_DESCRIPTION = "htmlDescription";
         public static final String COLUMN_URL = "url";
@@ -14,7 +14,7 @@ public class TableConfig {
 
         public static final String CREATE = ""
                 + "CREATE TABLE " + TABLE + "("
-                + COLUMN_ID + " INTEGER NOT NULL PRIMARY KEY,"
+                + COLUMN_ID + " TEXT NOT NULL PRIMARY KEY,"
                 + COLUMN_TITLE + " TEXT,"
                 + COLUMN_HTML_DESCRIPTION + " TEXT,"
                 + COLUMN_URL + " TEXT,"
